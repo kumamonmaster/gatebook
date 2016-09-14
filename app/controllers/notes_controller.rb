8473a8cf6f011640eefc7c52f818bc37ccc1c12a
kumamonmaster <fuusuke0715@gmail.com>
2016-09-14 04:52:33 +0000

    Add create action and make note model

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,11 @@
 class NotesController < ApplicationController
   def new
   end
+
+  def create
+    @note = Note.new
+    @note.title = params[:title]
+    @note.content = params[:content]
+    @note.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
   get '/about' => 'home#about'
-  get 'notes/new'
+  get '/notes/new'
+  post '/notes' => 'notes#create'
 end
