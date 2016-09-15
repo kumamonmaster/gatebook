e982ad461b58199ebb5187a4353986b30491ad3d
kumamonmaster <fuusuke0715@gmail.com>
2016-09-15 05:49:32 +0000

    Add has_many and belongs_to to user and note

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @notes = Note.where(user_id: @user.id)
+    @notes = @user.notes
   end
 
   # GET /users/new
